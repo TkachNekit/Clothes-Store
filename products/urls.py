3ca873596f16b6340a02ba89e7b65617d5489d23
Nikita Tkachenko <TkachLermontov@yandex.ru>
2024-01-24 14:54:16 +0500

    feat: linters and formatters added (flake8 and isort)

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,4 @@
-from django.urls import path, include
-
+from django.urls import path
 
 from products.views import ProductsListView, basket_add, basket_remove
 
@@ -12,4 +11,3 @@ urlpatterns = [
     path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
     path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove')
 ]
-
